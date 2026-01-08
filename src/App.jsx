feff6ba2d8239bcf6a80aae395ce21d01af11e17
bharatroy4D <bharatroy1005@gmail.com>
2026-01-08 14:26:46 +0600

    just use for interview

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center p-4 mx-auto font-bold text-3xl'><span className='text-yellow-400'>I</span> <span className='text-blue-800'>am</span> <span className='text-red-600'>ready</span> <span className='text-cyan-500'>for</span> <span className='text-green-600'>interview</span></h1>
+    </>
+  )
+}
+
+export default App
