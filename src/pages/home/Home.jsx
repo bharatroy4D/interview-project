dab90bfec7f0fbb0f40d55f4768d12547c8762c1
bharatroy4D <bharatroy1005@gmail.com>
2026-01-08 21:45:43 +0600

    Router path is setup done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-cyan-700 font-bold text-center text-4xl py-4'>This is Contacts</h1>
+        </div>
+    );
+};
+
+export default Home;
--- src/route/route.js
+++ src/route/route.js
@@ -1,6 +1,39 @@
+import Main from "../layout/Main";
+import Abouts from "../pages/abouts/Abouts";
+import Contacts from "../pages/contacts/Contacts";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/",
+                element: <Abouts />
+            },
+            {
+                path: "/",
+                element: <Contacts />
+            },
+            {
+                path: "/",
+                element: <Login />
+            },
+            {
+                path: "/",
+                element: <Register />
+            },
+        ]
 
+    }
 ])
 export default router;
