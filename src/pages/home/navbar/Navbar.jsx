c451e65989e4fbcdc3c24fce8e8a34b0d716ae13
bharatroy4D <bharatroy1005@gmail.com>
2026-01-28 13:02:55 +0600

    Testing navbar into close click outsite function setup

--- src/pages/home/navbar/Navbar.jsx
+++ src/pages/home/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { BsTranslate } from 'react-icons/bs';
 import { FiMenu } from 'react-icons/fi';
 import { IoMdClose } from 'react-icons/io';
@@ -6,6 +6,29 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
+    const menuRef = useRef(null);
+    // Close Menu clicking outside
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setOpen(false)
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside)
+    }, [])
+    // Close menu when clicking outside
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
     const routes = [
         { path: "/", label: "Home" },
         { path: "/abouts", label: "About" },
@@ -36,8 +59,10 @@ const Navbar = () => {
                     {open ? <IoMdClose className='text-xl' /> : <FiMenu className='text-xl' />}
                 </button>
                 {/* Mobile slide Menus */}
-                <div className={`fixed top-0 right-0 w-[65%] h-screen flex flex-col gap-1 bg-black px-5 py-3
-                    transform transition-transform duration-300 ${open? 'translate-x-0': 'translate-x-full'}`}>
+                <div
+                    ref={menuRef}
+                    className={`fixed top-0 right-0 w-[65%] h-screen flex flex-col gap-1 bg-black px-5 py-3
+                    transform transition-transform duration-300 ${open ? 'translate-x-0' : 'translate-x-full'}`}>
                     <div className='flex items-center justify-between mb-3'>
                         <h1 className='text-xl font-semibold'>Menu</h1>
                         <button onClick={() => setOpen(false)}>
