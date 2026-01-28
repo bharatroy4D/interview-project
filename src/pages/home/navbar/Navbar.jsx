8bb7458118a7108e91fdd250758ac8b25d20789a
bharatroy4D <bharatroy1005@gmail.com>
2026-01-28 12:36:42 +0600

    Testing navbar of responsive done

--- src/pages/home/navbar/Navbar.jsx
+++ src/pages/home/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BsTranslate } from 'react-icons/bs';
 import { FiMenu } from 'react-icons/fi';
 import { IoMdClose } from 'react-icons/io';
 import { Link } from 'react-router-dom';
@@ -11,7 +12,6 @@ const Navbar = () => {
         { path: "/contacts", label: "Contact" },
         { path: "/login", label: "Login" },
     ];
-
     return (
         <div>
             <div className='container flex justify-between px-10 py-4'>
@@ -35,6 +35,26 @@ const Navbar = () => {
                 <button className='md:hidden' onClick={() => setOpen(!open)}>
                     {open ? <IoMdClose className='text-xl' /> : <FiMenu className='text-xl' />}
                 </button>
+                {/* Mobile slide Menus */}
+                <div className={`fixed top-0 right-0 w-[65%] h-screen flex flex-col gap-1 bg-black px-5 py-3
+                    transform transition-transform duration-300 ${open? 'translate-x-0': 'translate-x-full'}`}>
+                    <div className='flex items-center justify-between mb-3'>
+                        <h1 className='text-xl font-semibold'>Menu</h1>
+                        <button onClick={() => setOpen(false)}>
+                            <IoMdClose className='text-white' />
+                        </button>
+                    </div>
+                    {
+                        routes.map(route => (
+                            <Link
+                                key={route.path}
+                                to={route.path}
+                                className=' '>
+                                {route.label}
+                            </Link>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     );
